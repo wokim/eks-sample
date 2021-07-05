a8e19fd670a7aa6431361d6214ccbe3bad6911b7
Kim, Wonshik (Alex) <wonshikkim.kr@gmail.com>
2021-07-05 10:53:07 +0900

    first commit

--- README.md
+++ README.md
@@ -1,14 +1,66 @@
-# Welcome to your CDK TypeScript project!
-
-This is a blank project for TypeScript development with CDK.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+# EKS Sample
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
+
+## Send a SSH public key to an instance
+
+As there are no SSH public keys deployed on this machine, you need to use EC2 Instance Connect with the command aws ec2-instance-connect send-ssh-public-key to provide your SSH public key.
+
+The following send-ssh-public-key example sends the specified SSH public key to the specified instance. The key is used to authenticate the specified user
+
+```sh
+# Each Linux instance launches with a default Linux system user account. The default user name is determined by the AMI that was specified when you launched the instance. For Amazon Linux 2 or the Amazon Linux AMI, the user name is `ec2-user`.
+
+aws ec2-instance-connect send-ssh-public-key \
+    --instance-id <instance-id> \
+    --instance-os-user <username> \
+    --availability-zone <az> \
+    --ssh-public-key file:///<pubkey path>
+
+# For example,
+aws ec2-instance-connect send-ssh-public-key \
+    --instance-id i-0efeed666c186b196 \
+    --instance-os-user ec2-user \
+    --availability-zone ap-northeast-2a \
+    --ssh-public-key file:///home/ec2-user/.ssh/id_rsa.pub
+```
+
+## SSH key-gen
+
+```sh
+ssh-keygen -t rsa
+```
+
+## To connect a bastion host
+
+```sh
+# Retrieve public DNS name to connect to bastion host
+aws ec2 describe-instances --instance-ids <instance-id> --query 'Reservations[].Instances[].PublicDnsName'
+
+# For example,
+aws ec2 describe-instances --instance-ids i-0efeed666c186b196 --query 'Reservations[].Instances[].PublicDnsName'
+
+# Connect SSH
+# After authentication, the public key is made available to the instance through the instance metadata for 60 seconds. During this time, connect to the instance using the associated private key:
+ssh ec2-user@<PublicDnsName>
+ssh ec2-user@ec2-13-125-37-72.ap-northeast-2.compute.amazonaws.com
+```
+
+## Cluster Autoscaler
+
+```sh
+kubectl apply -f cluster-autoscaler-autodiscover.yaml
+```
+
+## Termination Handler
+
+```sh
+kubectl apply -f https://github.com/aws/aws-node-termination-handler/releases/download/v1.3.1/all-resources.yaml
+```
--- lib/eks-sample-stack.ts
+++ lib/eks-sample-stack.ts
@@ -1,9 +1,123 @@
 import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class EksSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'eks-vpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 3,
+      // In order to save on NAT cost. Be aware you may be charged for cross-AZ data traffic instead.
+      natGateways: 2,
+      subnetConfiguration: [
+        { cidrMask: 24, name: 'public', subnetType: ec2.SubnetType.PUBLIC },
+        { cidrMask: 24, name: 'eks-worker-node', subnetType: ec2.SubnetType.PRIVATE },
+      ],
+      // In order for the private hosted zone to properly route traffic to your API server,
+      // your VPC must have enableDnsHostnames and enableDnsSupport set to true, and the DHCP options
+      // set for your VPC must include AmazonProvidedDNS in its domain name servers list.
+      // https://docs.aws.amazon.com/eks/latest/userguide/cluster-endpoint.html
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+    });
+
+    // Using Amazon EC2 Instance Connect for SSH access to your EC2 Instances
+    // https://aws.amazon.com/ko/blogs/compute/new-using-amazon-ec2-instance-connect-for-ssh-access-to-your-ec2-instances/
+    const sendSSHPublicKeyPolicyStatement = new iam.PolicyStatement();
+    sendSSHPublicKeyPolicyStatement.addResources(`arn:aws:ec2:${process.env.CDK_DEFAULT_REGION}:${process.env.CDK_DEFAULT_ACCOUNT}:instance/*`);
+    sendSSHPublicKeyPolicyStatement.addActions(
+      'ec2-instance-connect:SendSSHPublicKey',
+      'ec2:DescribeInstances'
+    );
+    sendSSHPublicKeyPolicyStatement.addCondition('StringEquals', {
+      'ec2:osuser': 'ec2-user'
+    });
+    sendSSHPublicKeyPolicyStatement.effect = iam.Effect.ALLOW;
+
+    const sendSSHPublicKeyPolicy = new iam.Policy(this, 'send-ssh-public-key-policy', {
+      policyName: 'SendSSHPublicKeyPolicy',
+      statements: [ sendSSHPublicKeyPolicyStatement ]
+    });
+
+    const bastion = new ec2.BastionHostLinux(this, 'eks-bastion', {
+      instanceName: 'eks-bastion',
+      vpc,
+      subnetSelection: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+    bastion.allowSshAccessFrom(ec2.Peer.anyIpv4());
+    sendSSHPublicKeyPolicy.attachToRole(bastion.role);
+
+    const cluster = new eks.Cluster(this, 'eks-sample', {
+      clusterName: 'eks-sample',
+      version: eks.KubernetesVersion.V1_20,
+      vpc,
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC }, { subnetType: ec2.SubnetType.PRIVATE }],
+      endpointAccess: eks.EndpointAccess.PUBLIC_AND_PRIVATE,
+      defaultCapacity: 0,
+      // Additional cluster security groups control communications from the Kubernetes control plane to compute resources in your account.
+      // It does not apply to worker nodes, but only to the control plane, so leave it as an empty security group.
+      securityGroup: new ec2.SecurityGroup(this, 'ControlPlaneAdditionalSecurityGroup', {
+        vpc,
+        description: 'EKS Control Plane Additional Security Group',
+      }),
+    });
+
+    // A cluster security group is designed to allow all traffic from the control plane and managed node groups to flow freely between each other.
+    // Allow SSH access to worker nodes.
+    cluster.clusterSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.icmpPing());
+    cluster.clusterSecurityGroup.addIngressRule(ec2.Peer.ipv4(vpc.vpcCidrBlock), ec2.Port.tcp(22));
+
+    // The following policy provides the minimum privileges necessary for Cluster Autoscaler to run.
+    // https://github.com/kubernetes/autoscaler/tree/master/cluster-autoscaler/cloudprovider/aws#iam-policy
+    // define the cluster autoscaler policy statements
+    // https://docs.aws.amazon.com/en_pv/eks/latest/userguide/cluster-autoscaler.html#ca-create-ngs
+    const clusterAutoscalerPolicyStatement = new iam.PolicyStatement();
+    clusterAutoscalerPolicyStatement.addResources('*');
+    clusterAutoscalerPolicyStatement.addActions(
+      'autoscaling:DescribeAutoScalingGroups',
+      'autoscaling:DescribeAutoScalingInstances',
+      'autoscaling:DescribeLaunchConfigurations',
+      'autoscaling:DescribeTags',
+      'autoscaling:SetDesiredCapacity',
+      'autoscaling:TerminateInstanceInAutoScalingGroup',
+      'ec2:DescribeLaunchTemplateVersions'
+    );
+    clusterAutoscalerPolicyStatement.effect = iam.Effect.ALLOW;
+
+    // Create the policy based on the statements
+    const clusterAutoscalerPolicy = new iam.Policy(this, 'cluster-autoscaler-policy', {
+      policyName: 'ClusterAutoscalerPolicy',
+      statements: [ clusterAutoscalerPolicyStatement ]
+    });
+
+    clusterAutoscalerPolicy.attachToRole(cluster.addNodegroupCapacity('eks-on-demand-capacity', {
+      nodegroupName: 'eks-on-demand-capacity',
+      instanceTypes: [
+        new ec2.InstanceType('m5.large'), // 1st priority
+        new ec2.InstanceType('m4.large')  // 2nd priority
+      ],
+      minSize: 2,
+      desiredSize: 2,
+      maxSize: 10,
+      capacityType: eks.CapacityType.ON_DEMAND,
+      diskSize: 20 // 20 GiB
+    }).role);
+
+    clusterAutoscalerPolicy.attachToRole(cluster.addNodegroupCapacity('eks-spot-capacity', {
+      nodegroupName: 'eks-spot-capacity',
+      instanceTypes: [
+        new ec2.InstanceType('m4.large'),
+        new ec2.InstanceType('m5.large'),
+        new ec2.InstanceType('m5a.large'),
+      ],
+      minSize: 1,
+      desiredSize: 3,
+      maxSize: 10,
+      capacityType: eks.CapacityType.SPOT,
+      diskSize: 20 // 20 GiB
+    }).role);
   }
 }
